06027efdf2cf1806569dd505c1860466cc510781
kiprasssiv <51048336+kiprasssiv@users.noreply.github.com>
2019-06-20 18:58:15 +0300

    Add files via upload

---
+++ mainCode.js
@@ -0,0 +1,116 @@
+var EasyButton = document.getElementsByClassName('levels')[0];
+var MediumButton = document.getElementsByClassName('levels')[1];
+var HardButton = document.getElementsByClassName('levels')[2];
+var fieldCards = [];
+var score = document.getElementById("PlayerScore");
+var playingCards = [];
+var crdF = document.getElementById("cardField");
+var firstCard;
+var secondCard;
+var firstFlipped;
+var currentScore;
+var flippedCards = [];
+var cardsOnThetable;
+
+function preparingCards(pairs){
+	cardsOnThetable = pairs * 2;
+	firstFlipped = 0;
+	crdF.innerHTML = "";
+	playingCards = [];
+	fieldCards = [];
+	currentScore = 100;
+	score.innerHTML = currentScore;
+	var cardSigns;
+	var cardPower = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+	var signArrayPlace;
+	var powerArrayPlace;
+	var newCard;
+	var i;
+	for(i = 0; i<pairs; i++){
+		cardSigns = ["S","H","D","C"];
+		signArrayPlace = Math.floor(Math.random()*(cardSigns.length));
+		powerArrayPlace = Math.floor(Math.random()*(cardPower.length));
+		newCard = cardPower[powerArrayPlace] + cardSigns[signArrayPlace];
+		fieldCards.push(newCard);
+		cardSigns.splice(signArrayPlace,1);
+		signArrayPlace = Math.floor(Math.random()*(cardSigns.length));
+		newCard = cardPower[powerArrayPlace] + cardSigns[signArrayPlace];
+		fieldCards.push(newCard);
+		cardPower.splice(powerArrayPlace,1);
+	}
+	displayCards(pairs);
+}
+
+function displayCards(pairs){
+	var i;
+	var randomizingGameSet;
+	var correctlyChosen = [];
+	var tableSize = document.getElementById("cardTable");
+	for(i = 0; i < pairs*2; i++){
+		correctlyChosen.push(0);
+		randomizingGameSet = Math.floor(Math.random()*(fieldCards.length));
+		playingCards.push(fieldCards[randomizingGameSet]);
+		fieldCards.splice(randomizingGameSet,1);
+	}
+	
+	for(i = 0; i < pairs*2; i++){
+		var card = document.createElement("BUTTON");
+		card.style.padding = "6%";
+		card.style.backgroundImage = "url('https://casino4you.com/wp-content/uploads/2015/03/peekredback.jpg')";
+		card.style.backgroundSize = "100%";
+		card.style.border = "none";
+		card.style.margin = "2%";
+		card.id = playingCards[i];
+		card.onclick = clickOne;
+		crdF.appendChild(card);
+		
+	}
+}
+
+function clickOne(){
+	var card = document.getElementById(this.id);
+	var dest = "url('images/"+ this.id +".png')";
+	card.style.backgroundImage = dest;
+	
+	if(firstFlipped == 0){
+		firstCard = this.id;
+		alert("first"+firstCard);
+		firstFlipped = 1;
+		flippedCards.push(this.id);
+	}
+	else{
+		firstFlipped = 0;
+		flippedCards.push(this.id);
+		secondCard = this.id;
+		alert("second"+secondCard);
+		
+		var currentTime = new Date().getTime();
+		while (currentTime + 1000000000000000000 >= new Date().getTime()) {
+		}
+		
+		if(firstCard[0] != secondCard[0])
+		{
+			currentScore = currentScore - 2;
+			score.innerHTML = currentScore;
+			
+			var i;
+			for(i=0;i<flippedCards.length;i++){
+				card = document.getElementById(flippedCards[i]);
+				card.style.backgroundImage = "url('https://casino4you.com/wp-content/uploads/2015/03/peekredback.jpg')";
+			}
+			flippedCards = [];
+			
+		}
+		
+	}
+	
+	if(flippedCards.length == cardsOnThetable)
+	{
+		alert("YOU WON");
+	}
+	if(currentScore == 0){
+		alert("YOU LOST");
+		preparingCards(cardsOnThetable / 2);
+	}
+	
+}
